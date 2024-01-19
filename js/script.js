045633e398fcdce7b1900fa4b1377f98ede2e580
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-01-19 14:55:40 +0800

    detail product done

--- js/script.js
+++ js/script.js
@@ -50,3 +50,27 @@ document.addEventListener("click", function (e) {
     shoppingCard.classList.remove("active");
   }
 });
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// click button close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// click outside modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
